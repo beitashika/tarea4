f08f6a429909297a3c8b9cd3dd6a3a972823ddad
Alumno <Alumno@INF715>
2013-02-18 13:07:35 +0100

    creación de ejercicio

---
+++ JavaApplication11/src/javaapplication11/JavaApplication11.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication11;
+
+/**
+ *
+ * @author Alumno
+ */
+public class JavaApplication11 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+              Alumno al1=new Alumno();
+        al1.muestraAlumno();
+ 
+    }
+}
